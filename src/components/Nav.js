0ab9a87c07da3928f95366bcb2460ea004ae39f0
Robert Czikkel <robertczikkel@Roberts-MacBook-Pro.local>
2022-03-23 01:17:51 +0000

    Pill added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -20,21 +20,34 @@ export default function Nav() {
   return (
     <header className="bg-slate-800">
         <nav className="xl:w-7/12 w-full m-auto flex flex-row justify-between items-center">
-            <div id="logo" className="md:w-28 w-24 px-2">
-                <img src="https://glopha.s3.eu-west-2.amazonaws.com/landing/logo-light.png" alt="logo"/>
+            {/* Logo */}
+            <div id="logo" className="md:w-28 w-24 px-2 hover:scale-105">
+                <a href="http://www.globalpharmacies.co.uk" target="_self"><img src="https://glopha.s3.eu-west-2.amazonaws.com/landing/logo-light.png" alt="logo"/></a>
             </div>
-            <div className="text-lg text-white hidden md:block px-2">
+            {/* Pill */}
+            <NavLink to="wishlist">
+                <div className="md:w-[30px] w-[25px] h-full relative hover:scale-105">
+                    <img className="relative top-1 right-3" src="https://glopha.s3.eu-west-2.amazonaws.com/landing/pill.png" alt="pill"/>
+                    <small className="absolute outline-white top-0 right-0 bg-red-600 rounded-full text-white md:text-[10px] text-[9px] px-2 py-1">2</small>
+                </div>
+            </NavLink>
+            {/* Links */}
+            <div className="text-md text-white hidden md:block px-2">
                 <ul className="flex flex-row">
                     <NavLink to='/' className={({ isActive }) => isActive ? activeClassName : undefined}><li className="md:py-1 h-1/2 rounded-md py-1 px-2">PRODUCTS</li></NavLink>
                     <NavLink to='contact' className={({ isActive }) => isActive ? activeClassName : undefined}><li className="md:py-1 h-1/2 rounded-md py-1 px-2">CONTACT</li></NavLink>
                 </ul>
             </div>
+
+            {/* Burger button */}
             <div id="burger" className="md:hidden w-[50px] h-[40px] flex flex-col justify-evenly items-start px-2">
                 <div className="h-[2px] w-full bg-white"></div>
                 <div className="h-[2px] w-3/4 bg-white"></div>
                 <div className="h-[2px] w-1/2 bg-white"></div>
             </div>
         </nav>
+
+        {/* Burger menu */}
         <div id="burgerMenu" className='absolute top-0 -translate-y-full transtion-all duration-1000 right-0 bg-gray-800 h-screen w-screen text-white'>
             <div id="cross" className="relative float-right w-[40px] h-[40px] px-2 ">
                 <div className="h-[2px] absolute top-8 right-2 w-full rotate-45 bg-white"></div>
