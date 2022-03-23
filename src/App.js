51c387878721fda76d0a8be4137206c5a2e1b790
Robert Czikkel <robertczikkel@Roberts-MacBook-Pro.local>
2022-03-23 12:30:08 +0000

    Routing changed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Products from './features/Products';
 import Detail from './components/Detail';
 import Contact from './features/Contact';
 import Wish from './features/Wish';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
 
   return (
-    <BrowserRouter> 
+    <HashRouter> 
       <div className="App">
         <Nav />
         <Routes>
@@ -19,9 +19,10 @@ function App() {
             <Route path='/:id' element={<Detail />}/>  
           <Route path='/contact' element={<Contact />}/>   
           <Route path='/wishlist' element={<Wish />}/> 
+          {/* <Route path='*' element={<Contact />}/> */}
         </Routes>     
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
